b5f8e34d0d9712ccd5dcab803a54c8e7684785f0
Scott Wattenbarger <scottwattenbarger@gmail.com>
2022-07-07 00:29:21 -0700

    adds questions to script.js

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+var questions = [ 
+    {
+        title: "How many heading tags are in HTML?",
+        choices: ["1", "2", "3", "6"],
+        answer: "6"
+    },
+    {
+        title: "Which HTML tag hyperlinks text?",
+        choices: ["p", "a", "div", "nav"],
+        answer: "a"
+    },
+    {
+        title: "Which hexadecimal combination displays the color red?",
+        choices: ["FF0000", "00FF00", "0000FF", "FFFF00"],
+        answer: "FF0000"
+    },
+    {
+        title: "Which HTML attribute points to images, audio files, and video clips?",
+        choices: ["href", "rel", "src", "type"],
+        answer: "src"
+    },
+    {
+        title: "In a group of radio buttons, multiple items can be selected.",
+        choices: ["True", "False"],
+        answer: "False"
+    }
+]
