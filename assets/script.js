6153f08eb991b5193167663767ab9e161ac88c68
Scott Wattenbarger <scottwattenbarger@gmail.com>
2022-07-08 12:38:59 -0700

    adds functional js to get timer, set score, input buttons for quiz, and if conditional for wrong or correct answers

--- assets/script.js
+++ assets/script.js
@@ -25,3 +25,120 @@ var questions = [
         answer: "False"
     }
 ]
+
+var showTime = document.getElementById("timeLeft");
+var score = 0;
+var currentQuestion = -1;
+var timeLeft = 0;
+var timer;
+
+function start(){
+    timeLeft = 40;
+    showTime.innerHTML = timeLeft
+
+    timer = setInterval(function(){
+        timeLeft--;
+        showTime.innerHTML = timeLeft;
+
+        if(timeLeft <= 0){
+            clearInterval(timer);
+            endGame();
+        }
+        
+    },1000);
+
+    next();
+}
+
+function next() {
+    currentQuestion++;
+
+    if (currentQuestion > questions.length - 1) {
+        endGame();
+        return;
+    }
+
+    var quizContent = "<h2>" + questions[currentQuestion].title + "</h2>"
+
+    for (var buttonLoop = 0; buttonLoop < questions[currentQuestion].choices.length; buttonLoop++) {        
+        var buttonCode = "<button onclick='[ANS]'>[CHOICE]</button>";         
+        buttonCode = buttonCode.replace("[CHOICE]", questions[currentQuestion].choices[buttonLoop]);        
+        
+        if (questions[currentQuestion].choices[buttonLoop] == questions[currentQuestion].answer) {           
+             buttonCode = buttonCode.replace("[ANS]", "correct()");
+             quizContent.textContent = "Correct";        
+        }   else { 
+               buttonCode = buttonCode.replace("[ANS]", "wrong()");      
+             }        
+             quizContent += buttonCode   
+    }
+    document.getElementById("quiz").innerHTML = quizContent;
+}
+
+function endGame() {
+    clearInterval(timer);
+
+    var quizContent = `
+        <h2>Game over!</h2>
+        <h3>You got a ` + score + ` /50!</h3>
+        <h3>That means you got ` + score / 10 + ` questions correct!</h3>
+        <input type="text" id="name" placeholder="Please enter your initals">
+        <button onclick="inputScore()">Set score!</button>`;
+
+        document.getElementById("quiz").innerHTML = quizContent;
+}
+
+
+function inputScore(){
+    localStorage.setItem("highscore", score);
+    localStorage.setItem("highscoreName", document.getElementsById('name').value);
+    getScore();
+}
+
+function getScore() {
+    var quizContent = `
+    <h2>` + localStorage.getItem("highscoreName") + `'s highscore is:</h2>
+    <h1>` + localStorage.getItem("highscore") + `</h1><br>
+    <button onclick="clearScore()">Clear score</button><button onclick="clearGame()">Play again!</button> `;
+
+    document.getElementById("quiz").innerHTML = quizContent;
+}
+
+function clearScore() {
+    localStorage.setItem("highscore", "");
+    localStorage.setItem("highscoreName", "");
+
+    clearGame();
+}
+
+
+function clearGame(){
+    clearInterval(timer);
+    score = 0;
+    currentQuestion = -1;
+    timeLeft = 0;
+    timer = null;
+
+    document.getElementById("timeLeft").innerHTML = timeLeft;
+
+    var quizContent = `
+    <h1>
+        Coding Quiz!
+    </h1>
+    <h3>
+        Click Start to play!
+    </h3>
+    <button onclick="start()">Start!</button>`;
+
+    document.getElementById("quiz").innerHTML = quizContent;
+}
+
+function wrong(){
+    timeLeft -= 15;
+    next()
+}
+
+function correct(){
+    score += 10;
+    next()
+}
